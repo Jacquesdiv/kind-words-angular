0cbfca47bdb5f2049d3fb71e61a4cd46021effce
Jacquesdiv <jacques@appstrax.tech>
2024-08-01 15:27:52 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  kindWordsApi: 'https://kind-words-api-dmneyh.codecapsules.co.za',
+  kindWordsApi: 'https://kind-words-api-xfoo.ovh-test.ccdns.co/',
 };
